364438e05eff0107fef6bdcf19b4b7a50c56d54f
HanYoung Lee <dkdlel644@naver.com>
2023-05-10 13:35:35 +0900

    BOJ_27323

---
+++ src/step10/BOJ_27323.java
@@ -0,0 +1,14 @@
+package step10;
+
+import java.util.Scanner;
+
+public class BOJ_27323 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		in.close();
+		
+		System.out.println(A*B);
+	}
+}
