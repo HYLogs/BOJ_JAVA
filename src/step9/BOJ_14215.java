583c8c044b2048e96f97d8dcdd9ad17ddffb3bab
HanYoung Lee <dkdlel644@naver.com>
2023-05-13 02:27:10 +0900

    BOJ_14215

---
+++ src/step9/BOJ_14215.java
@@ -0,0 +1,30 @@
+package step9;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_14215 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int[] arr = new int[3];
+		
+		arr[0] = in.nextInt();
+		arr[1] = in.nextInt();
+		arr[2] = in.nextInt();
+		in.close();
+		
+		Arrays.sort(arr);
+		
+		while(arr[2] >= (arr[0] + arr[1])) {
+			arr[2]--;
+		}
+		
+		
+		int result = 0;
+		for (int value: arr)
+			result += value;
+		
+		System.out.println(result);
+	}
+
+}
