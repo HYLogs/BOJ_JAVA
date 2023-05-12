fc796da9abe20b5ee12273f2a42537782d53311d
HanYoung Lee <dkdlel644@naver.com>
2023-05-13 03:08:45 +0900

    BOJ_24266

---
+++ src/step11/BOJ_24266.java
@@ -0,0 +1,14 @@
+package step11;
+
+import java.util.Scanner;
+
+public class BOJ_24266 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Long n = in.nextLong();
+		in.close();
+		
+		System.out.println(n*n*n + "\n" + 3);
+	}
+
+}
