0e10e5d222f252c31822235782688303b3ef4b98
HanYoung Lee <dkdlel644@naver.com>
2023-05-13 02:52:18 +0900

    BOJ_24264

---
+++ src/step11/BOJ_24264.java
@@ -0,0 +1,12 @@
+package step11;
+
+import java.util.Scanner;
+
+public class BOJ_24264 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Long n = in.nextLong();
+		in.close();
+		System.out.println(n*n + "\n" + 2);
+	}
+}
